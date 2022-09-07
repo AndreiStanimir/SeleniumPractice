a878f243ab44ffd777bf4697c6d12af08d9c96e5
Andrei Stănimir <andreistanimir@live.com>
2022-09-07 13:08:30 +0300

    Add project files.

---
+++ Selenium/Pages/VacanciesPage.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Pages
+{
+    public class VacanciesPage
+    {
+        IWebDriver driver;
+        private WebDriverWait wait;
+
+        public VacanciesPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
+            wait.Until(driver => driver.FindElement(By.Id("cookiescript_accept")));
+            PageFactory.InitElements(this.driver, this);
+        }
+
+        //[FindsBy(How = How.Id, Using = "sl", Priority = 0)]
+        [FindsBy(How = How.CssSelector, Using = ".col-lg-4 div:nth-of-type(2) #sl")]
+        [CacheLookup]
+        public IWebElement departmentsDropDown { get; set; }
+
+        [FindsBy(How.Id, "cookiescript_accept")]
+        public IWebElement acceptCookieButton { get; set; }
+
+        [FindsBy(How.CssSelector, ".h-100")]
+        IWebElement jobs;
+        public void SelectDepartment(string departmentName)
+        {
+
+            var departmentsDropDown = driver.FindElement(By.CssSelector(".col-lg-4 div:nth-of-type(2) #sl"));
+            departmentsDropDown.Click();
+            var departmentButton = departmentsDropDown.FindElement(By.XPath($"//a[.='{departmentName}']"));
+            //var department = departmentButton(e => e.Text.Equals(departmentName, StringComparison.CurrentCultureIgnoreCase));
+            if (departmentButton != null)
+                departmentButton.Click();
+        }
+        public int GetJobCount()
+        {
+            var jobsWebElements = driver.FindElements(By.CssSelector("a[class='card card-sm card-no-hover'"));
+            return jobsWebElements.Count();
+        }
+        public void AcceptCookies()
+
+        {
+            var acceptCookieButton = wait.Until(driver =>
+            {
+                var elem = driver.FindElement(By.Id("cookiescript_accept"));
+                if (elem.Displayed)
+                    return elem;
+                return null;
+                });
+            if (acceptCookieButton != null)
+                if (acceptCookieButton.Enabled && acceptCookieButton.Displayed)
+                    acceptCookieButton?.Click();
+        }
+    }
+}
---
+++ Selenium/UnitTest1.cs
@@ -0,0 +1,39 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using Selenium.Pages;
+using SeleniumExtras.PageObjects;
+using static System.Net.WebRequestMethods;
+
+namespace Selenium
+{
+    public class Tests
+    {
+        VacanciesPage vacanciesPage;
+        WebDriver driver;
+        [SetUp]
+        public void Setup()
+        {
+            driver = new FirefoxDriver(@"C:\Users\andreio\Downloads\geckodriver-v0.31.0-win64");
+            driver.Url = @"https://cz.careers.veeam.com/vacancies";
+            driver.Manage()
+                .Window
+                .Maximize();
+            driver.Manage()
+                .Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            
+            vacanciesPage = new VacanciesPage(driver);
+            vacanciesPage.AcceptCookies();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            //Assert.That(vacanciesPage.departmentsDropDown.Displayed^&)
+            vacanciesPage.SelectDepartment("Research & Development");
+            Assert.That(vacanciesPage.departmentsDropDown.Text, Contains.Substring("Research & Development"));
+            Assert.AreEqual(13, vacanciesPage.GetJobCount());
+        }
+    }
+}
